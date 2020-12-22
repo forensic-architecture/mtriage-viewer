5173cc9699b815e153d0eff7d99413c360cc74c5
Lachlan <Kermode>
2020-12-22 15:09:31 +0100

    basic copy update

--- src/components/About.vue
+++ src/components/About.vue
@@ -42,39 +42,51 @@ export default {
       content: [
         {
           title: `What is this?`,
-          description: `This is a demo instance of <a href="https://forensic-architecture.org/">Forensic Architecture's</a> <a href="https://github.com/forensic-architecture/mtriage-viewer">mtriage-viewer</a>. We use this interface to present the results of machine learning analysis to OSINT researchers.
-<p>For more information regarding how this has been applied in investigations, see our blog posts <a href="https://forensic-architecture.org/investigation/cv-in-triple-chaser">Computer Vision in Triple Chaser</a> and <a href="https://forensic-architecture.org/investigation/detecting-tear-gas">Detecting Tear Gas: Vision and Sound</a>.</p>`,
+          description: `This is a demo instance of <a href="https://forensic-architecture.org/">Forensic Architecture's</a> <a href="https://github.com/forensic-architecture/mtriage-viewer">mtriage-viewer</a>. The interface is designed as a workstation where our OSINT researchers can see the results of computational analysis on open source media, such as frame-by-frame classification of Youtube videos using machine learning classifiers.
+<p>You can read more about this methodology on our website:
+  <ul>
+  <li><a href="https://forensic-architecture.org/investigation/cv-in-triple-chaser">Computer Vision in Triple Chaser</a></li>
+  <li><a href="https://forensic-architecture.org/investigation/detecting-tear-gas">Detecting Tear Gas: Vision and Sound</a></li>
+  </ul>
+</p>
+  <p>Or see its output in the following investigations and exhibitions:
+  <ul>
+  <li><a href="https://forensic-architecture.org/investigation/the-battle-of-ilovaisk">The Battle of Ilovaisk</a></li>
+  <li><a href="https://forensic-architecture.org/investigation/triple-chaser">Triple Chaser</a></li>
+  <li><a href="https://forensic-architecture.org/investigation/model-zoo">Model Zoo</a></li>
+  <li><a href="https://forensic-architecture.org/investigation/tear-gas-in-plaza-de-la-dignidad">Tear Gas in Plaza de la Dignidad</a></li>
+  </ul>
+  </p>`,
         },
         {
           title: `FAQ`,
           description: `
           <div>
-          <strong><a href="https://github.com/forensic-architecture/mtriage">mtriage</a></strong> is an end-to-end framework that enables the retrieval and analysis of crowdsourced multimedia using machine learning.
-          </div>
-          <p>
-          An mtriage <strong>pipeline</strong> can be thought of as a sequence of data transformations which, ultimately, output artifacts that can be scrutinized and further actioned by a researcher. In short, it largely automates the labor-intensive process of annotating media inputs. 
-          </p>
-          <p>
-          Two of the main components of an mtriage pipeline are: <strong>selectors</strong> and <strong>analyzers</strong>: the former pertain to the retrieval of data, whereas the latter relates to determining regions of interest within the media itself.
-          </p>
-          <p>
-          The output of an mtriage pipeline is called a <strong>batch</strong>. A batch highlights regions of interest resulting from the execution of an mtriage pipeline: if the pipeline pertains to identifying objects of interest using Computer Vision, the appropriate <strong>viewer</strong> will present the relevant results in this interface.
-          </p>
-          <p>
-          For more detail on the rationale behind developing mtriage, check out <a href="https://github.com/forensic-architecture/mtriage/blob/main/docs/overview.md">our overview document.</a>
-          </p>`,
+          <p><strong>What do 'batches' represent?</strong></p>
+          <p>An mtriage <strong>batch</strong> can be understood as what is produced by a sequence of data transformations.It is produced as the result of taking some media and processing it via modules which we call <strong>analyzers</strong>. This interface is designed to helped automate some of the more labor-intensive processes of open source research.</p>
+
+          <p><strong>How are batches produced?</strong></p>
+          <p><strong><a href="https://github.com/forensic-architecture/mtriage">mtriage</a></strong> is a framework for orchestrating computational analysis. It includes both modules that can search and retrieve media from various online platforms, and modules that can analyse that media with machine learning classifiers or other analytic techniques.</p>
+          <p>For more detail on the rationale behind developing mtriage, check out <a href="https://github.com/forensic-architecture/mtriage/blob/main/docs/overview.md">its documentation.</a></p>`,
         },
         {
           title: `How can I contribute?`,
           description: `<div>We welcome input, discussion, and contributions from open-source developers. One can check out the mtriage <a href="https://github.com/forensic-architecture/mtriage">repository</a> on Github.</div>
-          <p>The source code for our website can be found at: TODO.</p>
+          <p>This website is powered by <a href="https://github.com/forensic-architecture/mtriage-server">mtriage-server</a> and <a href="https://github.com/forensic-architecture/mtriage-viewer">mtriage-viewer</a> in tandem.</p>
           <p>If you are interested in helping us develop additional selectors and analyzers, please consider joining <a href="https://discord.gg/FJ4XsCg">the conversation on Discord.</a></p>`,
         },
         {
           title: `People`,
-          description: `<div>
-          We are a team of software researchers, journalists, and academics. Our extended community members help us with the development of our open-source frameworks.</div>
-          <p>Below are our core members:</p>`,
+          description: `<div>Though stewarded by <a href="https://forensic-architecture.org">Forensic Architecture</a>, the mtriage ecosystem is developed and maintained by a broader team of software researchers, journalists, and academics. The extended community is integral to mtriage's development and use.</div>
+          <p>Active contributors include:</p>
+        <ul>
+          <li><a href="https://lachlankermode.com">Lachie Kermode</a></li>
+          <li><a href="https://forensic-architecture.org/about/team/member/zac-ioannidis">Zac Ioannidis</a></li>
+          <li><a href="https://github.com/christegho">Chris Tegho</a></li>
+          <li><a href="https://github.com/ashwindcruz">Ashwin D. Cruz</a></li>
+          <li><a href="https://github.com/RibenaMapleSyrup">Sean Greaves</a></li>
+          <li><a href="https://forensic-architecture.org/about/team/member/ebrahem-farooqui">Ebrahem Farooqui</a></li>
+        </ul>`,
         },
       ],
     };
